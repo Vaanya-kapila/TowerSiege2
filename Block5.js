c33243ec68ec033fcf9a47eb1f59775c0f8af107
Vaanya-kapila <83177828+Vaanya-kapila@users.noreply.github.com>
2021-06-24 20:27:36 -0800

    Add files via upload

---
+++ Block5.js
@@ -0,0 +1,25 @@
+class Block5 extends BaseClass {
+  constructor(x,y){
+    super(x,y,100,60);
+    this.Visibility = 255;
+  }
+
+  display(){
+    if(this.body.speed<3){
+      super.display();
+    }
+    else{
+      World.remove(world, this.body);
+      push();
+      this.Visibility = this.Visibility - 5;
+      tint(255,this.Visibility)
+      pop();
+    }
+  }
+
+  score(){
+    if(this.Visibility<0 && this.Visibility>-1005){
+      score++;
+    }
+  }
+};
